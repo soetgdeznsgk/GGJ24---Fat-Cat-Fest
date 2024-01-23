087c3353ffb26bee5da0c4437bfff39757438dcf
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2024-01-23 17:59:24 -0500

    Animaciones bonitas
    pero tiene un bug raro la letra S

--- Escenas/Menus/Buttons/Button.gd
+++ Escenas/Menus/Buttons/Button.gd
@@ -1,13 +1,33 @@
 extends Button
 @onready var anim:AnimationPlayer = $AnimationPlayer
-@onready var tween : Tween
-@onready var leftMargin = $PlayButton.h_separation
+var seeker = 0
+var backwards = false
+var forward = false
+# TODO corregir que la S parpadea con la animación
 func _on_mouse_entered():
-	print(leftMargin)
-		
+	if anim.is_playing():
+		seeker = anim.current_animation_position
+	else:
+		seeker = 0
+	if !forward:
+		anim.play("slide")
+		forward = true
+		backwards = false
+		anim.seek(seeker)
+
 func _on_mouse_exited():
-	print(leftMargin)
+	if anim.is_playing():
+		seeker = anim.current_animation_position
+	else:
+		seeker = 0
+	if !backwards:
+		anim.seek(seeker)
+		anim.play_backwards("slide")
+		forward = false
+		backwards = true
 
+func _on_focus_entered() -> void:
+	_on_mouse_entered()
 
-
-
+func _on_focus_exited() -> void:
+	_on_mouse_exited()

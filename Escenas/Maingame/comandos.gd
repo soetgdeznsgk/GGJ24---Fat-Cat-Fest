57b41aad399baf37b65a092f8b47ea00cf0d78fc
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2024-01-23 10:44:41 -0500

    Refinamiento del sistema, codigo y soporte para dos mandos

--- Escenas/Maingame/comandos.gd
+++ Escenas/Maingame/comandos.gd
@@ -2,87 +2,79 @@ extends Node2D
 
 @export var jugador = 1
 var diccionario := {}
-var textureList = [ load("res://Sprites/Comandos/flechaArriba.png"), \
+var listaTexturas = [ load("res://Sprites/Comandos/flechaArriba.png"), \
 load("res://Sprites/Comandos/FlechaAbajo.png"), \
 load("res://Sprites/Comandos/FlechaIzquierda.png"), \
 load("res://Sprites/Comandos/FlechaDerecha.png") ]
 
+var ultimoInputRegistrado = null
+
 @onready var anim = $AnimationPlayer
 @onready var comandoNodos = [$Comando0, $Comando1, $Comando2, $Comando3]
 var comandos : Array = []
-var esperarError := false
 var comandosConFlechas : Array = []
-enum {
-	Arriba,
-	Abajo,
-	Izquierda,
-	Derecha
-}
-
+var permitirEntradas = true
+var devices
 func _ready() -> void:
 	# Dependiendo del jugador tiene ciertas teclas para el physic process
 	if jugador == 1:
-		diccionario[Arriba] = "ArribaPj1"
-		diccionario[Abajo] = "AbajoPj1"
-		diccionario[Izquierda] = "IzquierdaPj1"
-		diccionario[Derecha] = "DerechaPj1"
+		diccionario[Enums.Arriba] = "ArribaPj1"
+		diccionario[Enums.Abajo] = "AbajoPj1"
+		diccionario[Enums.Izquierda] = "IzquierdaPj1"
+		diccionario[Enums.Derecha] = "DerechaPj1"
 	else:
-		diccionario[Arriba] = "ArribaPj2"
-		diccionario[Abajo]  = "AbajoPj2"
-		diccionario[Izquierda] = "IzquierdaPj2"
-		diccionario[Derecha] = "DerechaPj2"
+		diccionario[Enums.Arriba] = "ArribaPj2"
+		diccionario[Enums.Abajo]  = "AbajoPj2"
+		diccionario[Enums.Izquierda] = "IzquierdaPj2"
+		diccionario[Enums.Derecha] = "DerechaPj2"
 		
-	# deberian llegar en la comida
-	comandos = [Arriba,Abajo,Izquierda,Derecha,Arriba,Abajo,Izquierda,Derecha]
-	comandosConFlechas = [Arriba,Abajo,Izquierda,Derecha,Arriba,Abajo,Izquierda,Derecha]
+	# TODO deberian llegar en la comida
+	comandos = [Enums.Arriba,Enums.Abajo,Enums.Izquierda,Enums.Derecha]
+	comandosConFlechas = [Enums.Arriba,Enums.Abajo,Enums.Izquierda,Enums.Derecha]
+	
+	# Colocar las primeras 3 flechas que llegan de la comida
 	for i in range(3):
 		var ultimo = comandos.pop_front()
-		comandoNodos[i].texture = textureList[ultimo]
+		comandoNodos[i].texture = listaTexturas[ultimo]
 
-func _physics_process(delta: float) -> void:
-	# Quiza falta input buffering
-	# Lo hace bien
+func _physics_process(_delta: float) -> void:
+	# Input buffering para teclado
+	if Input.is_action_just_pressed(diccionario[Enums.Arriba]):
+		ultimoInputRegistrado = Enums.Arriba
+	elif Input.is_action_just_pressed(diccionario[Enums.Abajo]):
+		ultimoInputRegistrado = Enums.Abajo
+	elif Input.is_action_just_pressed(diccionario[Enums.Izquierda]):
+		ultimoInputRegistrado = Enums.Izquierda
+	elif Input.is_action_just_pressed(diccionario[Enums.Derecha]):
+		ultimoInputRegistrado = Enums.Derecha
+	
+	
+	# Si está spameando entonces va mas rápido la animación
+	if ultimoInputRegistrado != null and anim.is_playing() \
+	and anim.assigned_animation == "scroll_izquierda":
+		anim.speed_scale = 4
+	# Si ya no hay buffer vuelve la animación a ser lenta
+	if ultimoInputRegistrado == null and anim.assigned_animation == "scroll_izquierda":
+		anim.speed_scale = 1
+	# Acá es donde verifica si quedan comandos en la lista y verifica justo cuando
+	# lo permita la entrada según el buffer
 	if comandosConFlechas.size() > 0:
-		if !esperarError:
-			var correcta = comandosConFlechas[0]
-			if Input.is_action_just_pressed(diccionario[Arriba]):
-				if correcta == Arriba:
-					comandosConFlechas.pop_front()
-					if anim.is_playing():
-						animacionRapida()
-					actualizar_flechas()
-				else:
-					error_flechas()
-			elif Input.is_action_just_pressed(diccionario[Abajo]):
-				if correcta == Abajo:
-					comandosConFlechas.pop_front()
-					if anim.is_playing():
-						animacionRapida()
-					actualizar_flechas()
-				else:
-					error_flechas()
-			elif Input.is_action_just_pressed(diccionario[Izquierda]):
-				if correcta == Izquierda:
-					comandosConFlechas.pop_front()
-					print(comandosConFlechas)
-					if anim.is_playing():
-						animacionRapida()
-					actualizar_flechas()
-				else:
-					error_flechas()
-			elif Input.is_action_just_pressed(diccionario[Derecha]):
-				if correcta == Derecha:
-					comandosConFlechas.pop_front()
-					if anim.is_playing():
-						animacionRapida()
-					actualizar_flechas()
-				else:
-					error_flechas()
+		if ultimoInputRegistrado != null and permitirEntradas:
+			verificarCorrecta(ultimoInputRegistrado)
+			ultimoInputRegistrado = null
+			permitirEntradas = false
 			
 
-func animacionRapida():
-	anim.stop()
-	anim.play("RESET")
+func verificarCorrecta(Direccion):
+	var correcta = comandosConFlechas[0]
+	if correcta == Direccion:
+		actualizar_flechas()
+	else:
+		error_flechas()
+
+func reemplazarTexturas():
+	comandos.pop_front()
+	comandosConFlechas.pop_front()
 	comandoNodos[0].texture = comandoNodos[1].texture
 	comandoNodos[1].texture = comandoNodos[2].texture
 	comandoNodos[2].texture = comandoNodos[3].texture
@@ -91,39 +83,30 @@ func animacionRapida():
 		comandoNodos[i].self_modulate = Color(1,1,1,1)
 
 func actualizar_flechas():
+	# le pone textura a la nueva flecha por salir
 	if comandos.size() > 0:
-		var ultimo = comandos.pop_front()
-		comandoNodos[3].texture = textureList[ultimo]
+		var ultimo = comandos[0]
+		comandoNodos[3].texture = listaTexturas[ultimo]
 	else:
-		var ultimo = null
 		comandoNodos[3].texture = null
-	if anim.is_playing():
-		anim.play("scroll_izquierda",-1,5)
-	else:
-		anim.play("scroll_izquierda",-1,1)
+	anim.play("scroll_izquierda")
 
 func error_flechas():
-	esperarError = true
+	# Lo demora arto si se equivocó de manera greedy
 	if anim.is_playing():
 		anim.speed_scale = 5
 		await anim.animation_finished 
 		anim.speed_scale = 0.3
-		anim.play("error_flecha")
-	else:
+		anim.queue("error_flecha")
+	else: # lo demora menos si se equivocó en el mismo
 		anim.speed_scale = 0.7
-		anim.play("error_flecha")
+		anim.queue("error_flecha")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "scroll_izquierda":
-		comandoNodos[0].texture = comandoNodos[1].texture
-		comandoNodos[1].texture = comandoNodos[2].texture
-		comandoNodos[2].texture = comandoNodos[3].texture
-		for i in comandoNodos.size():
-			comandoNodos[i].position = Vector2((i+1)*128, 51)
-			comandoNodos[i].self_modulate = Color(1,1,1,1)
-	if anim_name == "error_flecha":
-		esperarError = false
+		reemplazarTexturas()
+	elif anim_name == "error_flecha":
 		anim.speed_scale = 1
-			
+	permitirEntradas = true
 	
-	
+

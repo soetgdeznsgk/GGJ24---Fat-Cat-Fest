49c79cada95c6a1c4a6de30d853e36e8cc7370ce
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2024-01-22 20:58:29 -0500

    Refinamiento 2 y ya funciona con los inputs de los jugadores
    Falta unirlo con las comidas y dar señáles

--- Escenas/Maingame/comandos.gd
+++ Escenas/Maingame/comandos.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
-var textureEnum = [ load("res://Sprites/Comandos/flechaArriba.png"), \
+@export var jugador = 1
+var diccionario := {}
+var textureList = [ load("res://Sprites/Comandos/flechaArriba.png"), \
 load("res://Sprites/Comandos/FlechaAbajo.png"), \
 load("res://Sprites/Comandos/FlechaIzquierda.png"), \
 load("res://Sprites/Comandos/FlechaDerecha.png") ]
 
 @onready var anim = $AnimationPlayer
-
 @onready var comandoNodos = [$Comando0, $Comando1, $Comando2, $Comando3]
 var comandos : Array = []
 var esperarError := false
-
+var comandosConFlechas : Array = []
 enum {
 	Arriba,
 	Abajo,
@@ -18,35 +19,88 @@ enum {
 	Derecha
 }
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Dependiendo del jugador tiene ciertas teclas para el physic process
+	if jugador == 1:
+		diccionario[Arriba] = "ArribaPj1"
+		diccionario[Abajo] = "AbajoPj1"
+		diccionario[Izquierda] = "IzquierdaPj1"
+		diccionario[Derecha] = "DerechaPj1"
+	else:
+		diccionario[Arriba] = "ArribaPj2"
+		diccionario[Abajo]  = "AbajoPj2"
+		diccionario[Izquierda] = "IzquierdaPj2"
+		diccionario[Derecha] = "DerechaPj2"
+		
 	# deberian llegar en la comida
-	comandos = [Arriba,Abajo,Izquierda,Derecha,Abajo,Izquierda,Derecha,Arriba,Abajo,Arriba,Abajo]
+	comandos = [Arriba,Abajo,Izquierda,Derecha,Arriba,Abajo,Izquierda,Derecha]
+	comandosConFlechas = [Arriba,Abajo,Izquierda,Derecha,Arriba,Abajo,Izquierda,Derecha]
 	for i in range(3):
 		var ultimo = comandos.pop_front()
-		comandoNodos[i].texture = textureEnum[ultimo]
+		comandoNodos[i].texture = textureList[ultimo]
 
 func _physics_process(delta: float) -> void:
 	# Quiza falta input buffering
 	# Lo hace bien
-	if Input.is_action_just_pressed("ui_accept"):
-		actualizar_flechas()
-	# Se equivoca
-	if Input.is_action_just_pressed("ui_cancel"):
-		error_flechas()
+	if comandosConFlechas.size() > 0:
+		if !esperarError:
+			var correcta = comandosConFlechas[0]
+			if Input.is_action_just_pressed(diccionario[Arriba]):
+				if correcta == Arriba:
+					comandosConFlechas.pop_front()
+					if anim.is_playing():
+						animacionRapida()
+					actualizar_flechas()
+				else:
+					error_flechas()
+			elif Input.is_action_just_pressed(diccionario[Abajo]):
+				if correcta == Abajo:
+					comandosConFlechas.pop_front()
+					if anim.is_playing():
+						animacionRapida()
+					actualizar_flechas()
+				else:
+					error_flechas()
+			elif Input.is_action_just_pressed(diccionario[Izquierda]):
+				if correcta == Izquierda:
+					comandosConFlechas.pop_front()
+					print(comandosConFlechas)
+					if anim.is_playing():
+						animacionRapida()
+					actualizar_flechas()
+				else:
+					error_flechas()
+			elif Input.is_action_just_pressed(diccionario[Derecha]):
+				if correcta == Derecha:
+					comandosConFlechas.pop_front()
+					if anim.is_playing():
+						animacionRapida()
+					actualizar_flechas()
+				else:
+					error_flechas()
+			
+
+func animacionRapida():
+	anim.stop()
+	anim.play("RESET")
+	comandoNodos[0].texture = comandoNodos[1].texture
+	comandoNodos[1].texture = comandoNodos[2].texture
+	comandoNodos[2].texture = comandoNodos[3].texture
+	for i in comandoNodos.size():
+		comandoNodos[i].position = Vector2((i+1)*128, 51)
+		comandoNodos[i].self_modulate = Color(1,1,1,1)
 
 func actualizar_flechas():
-	if !esperarError:
-		if comandos.size() > 0:
-			var ultimo = comandos.pop_front()
-			comandoNodos[3].texture = textureEnum[ultimo]
-		else:
-			var ultimo = null
-			comandoNodos[3].texture = null
-		if anim.is_playing():
-			anim.play("scroll_izquierda",-1,5)
-		else:
-			anim.play("scroll_izquierda",-1,1)
+	if comandos.size() > 0:
+		var ultimo = comandos.pop_front()
+		comandoNodos[3].texture = textureList[ultimo]
+	else:
+		var ultimo = null
+		comandoNodos[3].texture = null
+	if anim.is_playing():
+		anim.play("scroll_izquierda",-1,5)
+	else:
+		anim.play("scroll_izquierda",-1,1)
 
 func error_flechas():
 	esperarError = true

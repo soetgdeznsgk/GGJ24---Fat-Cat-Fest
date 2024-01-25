40af6d126ebfa61e431ae6d484cc7f4fdaff88f9
Santiago Reyes <sreyeso@unal.edu.co>
2024-01-25 12:59:45 -0500

    publico gatos
    spawn cheer
    TODO:
    linkear las animaciones al resto de cosas, sonido (maybe yamato faker)

---
+++ Escenas/Maingame/Publico/GatosPublico.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+# An array of different cat X positions
+var catXPositions: Array = [ -10, 140, 290, 440, 590, 740, 890, 1040, 1190 ]
+
+# Array to store references to the instantiated cat instances
+var catInstances: Array = []
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	instantiateRandomCats()
+
+func _process(delta: float) -> void:
+	# Check if the Enter key is pressed
+	if Input.is_action_just_pressed("ui_accept"):
+		# Call the triggerRandomCheer function
+		triggerRandomCheer()
+
+func instantiateRandomCats() -> void:
+	# Generate a random number of cats
+	#TODO : dependiendo de la diff y todo menos o mas publico
+	var numberOfCats = randi_range(6, 10)
+
+	# Shuffle the array of cat X positions to randomize their order
+	catXPositions.shuffle()
+
+	# Limit the number of cats to the size of the array
+	numberOfCats = min(numberOfCats, catXPositions.size())
+
+	for i in range(numberOfCats):
+		# Instantiate the cat scene
+		var catInstance = preload("res://Escenas/Maingame/Publico/GatoPublico.tscn").instantiate()
+
+		# Set the X position of the cat from the shuffled array
+		catInstance.position.x = catXPositions[i]
+
+		# Generate a random Y position within the range of -5 to 5 from a base Y position
+		catInstance.position.y = randi_range(763, 771)
+
+		# Add the cat to the scene
+		add_child(catInstance)
+		
+		# Add the cat instance to the array
+		catInstances.append(catInstance)
+
+# Function to trigger a cheer animation randomly for some cats
+func triggerRandomCheer() -> void:
+	for cat in catInstances:
+		# Generate a random number to determine if the cat cheers (50% chance)
+		if randf() < 0.5:
+			# Access the AnimationPlayer in each cat and play the cheer animation
+			var animPlayer = cat.get_node("AnimationPlayer")
+			if animPlayer != null:
+				animPlayer.play("cheer")

261d6cf1cfacfa09a3d5581670b129ea7c23ddc3
emiliano <68301161+soetgdeznsgk@users.noreply.github.com>
2024-01-26 11:47:14 -0500

    arreglos pequeños de UI, desactivación de comandos al lanzar SP

--- Escenas/Maingame/Maingame.gd
+++ Escenas/Maingame/Maingame.gd
@@ -4,3 +4,10 @@ func _ready() -> void:
 	if Eventos.singleplayer:
 		var bot = preload("res://Logica/cpu_jugador.tscn").instantiate()
 		add_child(bot)
+		InputMap.action_erase_events("ArribaPj1") 
+		InputMap.action_erase_events("AbajoPj1")
+		InputMap.action_erase_events("IzquierdaPj1")
+		InputMap.action_erase_events("DerechaPj1")
+	else:
+		# en caso de que se ejecute luego de una partida SP, reestablecer las acciones de Pj1
+		pass
--- Escenas/Maingame/names.gd
+++ Escenas/Maingame/names.gd
@@ -2,11 +2,12 @@ extends Node
 
 var name_player1
 var name_player2
-var nameinitial : Array = ["Silly", "Tiny", "Huge", "The", "Cute", "Nonsensical", "Addictive", "Smol", "Sad", "Baby", "Eepy", "Anxious", "Crazy", "Golden"]
-var namefinal : Array = ["Car", "Kitty", "WAR CRIMINAL", "Chonker", "Purrer", "Bingus", "Microplastic Enjoyer", "Mulch", "Yippie", "Nyan", "Hobo", "Messi", "Karen Adopted"]
+var nameinitial : Array = ["Silly", "Tiny", "Huge", "The", "Cute", "Nonsensical", "Addictive", "Smol", "Sad", "Baby", "Eepy", "Anxious", "Crazy", "Golden", "Ex-convict"]
+var namefinal : Array = ["Car", "Kitty", "WAR CRIMINAL", "Chonker", "Purrer", "Bingus", "Microplastic Enjoyer", "Mulch", "Yippie", "Nyan", "Hobo", "Messi", "Karen Adopted", "Paw Sniffer"]
 
 # Al iniciarse el singleton toma un nombre random, tambien al darle play al juego
 # lo pongo aca para que no se rompa al probar escenas solitas
 func _ready() -> void:
 	Names.name_player1 = Names.nameinitial.pick_random() + " " + Names.namefinal.pick_random()
 	Names.name_player2 = Names.nameinitial.pick_random() + " " + Names.namefinal.pick_random()
+	pass

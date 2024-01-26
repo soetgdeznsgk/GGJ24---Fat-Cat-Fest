3c3b187f64fe1832bcff42730fa5ac079a0ad6cd
SantBl18 <sballenl@unal.edu.co>
2024-01-26 14:43:01 -0500

    Cambio de comandos

--- Logica/BuñueloYNatilla.gd
+++ Logica/BuñueloYNatilla.gd
@@ -1,3 +1,3 @@
 extends Sprite2D
 var nombre= "BuñueloyNatilla"
-var moveset = [Enums.Izquierda, Enums.Arriba, Enums.Derecha, Enums.Izquierda, Enums.Derecha, Enums.Arriba, Enums.Arriba, Enums.Izquierda, Enums.Abajo]
+var moveset = [Enums.Abajo, Enums.Arriba, Enums.Derecha, Enums.Derecha, Enums.Izquierda, Enums.Derecha, Enums.Abajo, Enums.Izquierda, Enums.Arriba]

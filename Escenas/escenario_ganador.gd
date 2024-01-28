4468f6ef8dfdadc9945e35561e1bdacd3ede0d61
SantBl18 <sballenl@unal.edu.co>
2024-01-28 12:52:23 -0500

    Reproducción de audios de nombres en la escena de ganador

--- Escenas/escenario_ganador.gd
+++ Escenas/escenario_ganador.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var soundQueue = []
 
 func _ready() -> void:
 	$AnimationPlayer.play("mostrar_ganador")
@@ -11,6 +12,10 @@ func _ready() -> void:
 		await get_tree().create_timer(.2).timeout
 		$Gato1.play("loop_victoria")
 		$Gato2.play("loop_derrota")
+		soundQueue.append(load(Names.dirAudioInitial1))
+		soundQueue.append(load(Names.dirAudioFinal1))
+		$AudioStreamPlayer.stream = soundQueue.pop_front()
+		$AudioStreamPlayer.play()
 	elif Eventos.ganador == 2:
 		$Label.text = "WINNER OF CAT FAST FEST IS ... " + Names.name_player2
 		$LabelNombre.text = Names.name_player2
@@ -23,6 +28,10 @@ func _ready() -> void:
 		await get_tree().create_timer(.2).timeout
 		$Gato1.play("loop_derrota")
 		$Gato2.play("loop_victoria")
+		soundQueue.append(load(Names.dirAudioInitial2))
+		soundQueue.append(load(Names.dirAudioFinal2))
+		$AudioStreamPlayer.stream = soundQueue.pop_front()
+		$AudioStreamPlayer.play()
 func _process(delta: float) -> void:
 	pass
 
@@ -35,3 +44,8 @@ func set_sfx_random_anuncia():
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas/Menus/menu_principal.tscn")
 	queue_free()
+
+func _on_audio_stream_player_finished():
+	$AudioStreamPlayer.stream = soundQueue.pop_front()
+	$AudioStreamPlayer.play()
+	pass # Replace with function body.

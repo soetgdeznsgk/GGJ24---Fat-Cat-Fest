919f498fa643fadf81e3ec1a45bf1495eec94f43
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2024-01-28 09:50:16 -0500

    Bug de que darse duro pusiera el ganador incorrecto

--- Escenas/Eventos/darseduro/evento_darse_duro.gd
+++ Escenas/Eventos/darseduro/evento_darse_duro.gd
@@ -5,11 +5,12 @@ signal p2_can_hit
 func _on_timer_finalizar_evento_timeout() -> void:
 	if $Gato.conteoGolpesRecibidos == $Gato2.conteoGolpesRecibidos:
 		Eventos.finalEvento.emit(0)
-	elif $Gato.conteoGolpesRecibidos > $Gato2.conteoGolpesRecibidos:
-		Eventos.finalEvento.emit(1)
 	elif $Gato.conteoGolpesRecibidos < $Gato2.conteoGolpesRecibidos:
+		Eventos.finalEvento.emit(1)
+	elif $Gato.conteoGolpesRecibidos > $Gato2.conteoGolpesRecibidos:
 		Eventos.finalEvento.emit(2)
 	queue_free()
+	print($Gato.conteoGolpesRecibidos, " ", $Gato2.conteoGolpesRecibidos )
 
 func relay_p2_can_hit() -> void:
 	p2_can_hit.emit()

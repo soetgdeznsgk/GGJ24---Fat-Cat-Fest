5784a5c7fc273efaed2d50b0e4e86671ac9a21b0
SantBl18 <sballenl@unal.edu.co>
2024-01-27 21:35:40 -0500

    Mas recetas añadidas

---
+++ Escenas/Recetas/BandejaPaisa.gd
@@ -0,0 +1,3 @@
+extends Sprite2D
+var nombre= "BandejaPaisa"
+var moveset = [Enums.Izquierda, Enums.Derecha, Enums.Arriba, Enums.Arriba, Enums.Arriba, Enums.Derecha, Enums.Abajo, Enums.Izquierda, Enums.Abajo]
---
+++ Escenas/Recetas/Bocadillo.gd
@@ -0,0 +1,3 @@
+extends Sprite2D
+var nombre= "Bocadillo"
+var moveset = [Enums.Arriba, Enums.Abajo, Enums.Arriba, Enums.Izquierda, Enums.Arriba, Enums.Abajo, Enums.Izquierda, Enums.Derecha, Enums.Derecha]

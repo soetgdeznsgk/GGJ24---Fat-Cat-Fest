5da0da18c0c03a47125da6c8cd55dbf161a366c7
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2024-03-22 18:57:03 -0500

    HELL YEA

--- Logica/Globales/MultiplayerControl.gd
+++ Logica/Globales/MultiplayerControl.gd
@@ -4,7 +4,7 @@ extends Node
 var isHost : bool
 var address: String
 var hostingId = 1
-var clientId  
+var clientId = null
 var mp : MatchaRoom = null
 signal new_event(args)
 
@@ -23,6 +23,8 @@ func new_event_handler(args):
 	new_event.emit(args)
 
 func new_peer(_id, peer):
+	if clientId == null:
+		clientId = peer._id
 	var tt = "[Server] Peer joined (id=%s)\n and with id int =%s" % [peer.id,_id]
 	print(tt)
 	peer.emit_new_event.connect(new_event_handler)
